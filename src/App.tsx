93ad2b41c12f43da77d149a368573c68f550c905
Eugene <88931804+eugenevve@users.noreply.github.com>
2026-07-09 17:35:29 +0600

    Adding the react-router library and adding its components to the main page along with the Head component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 import type { FC } from "react";
+import { BrowserRouter, Routes } from "react-router";
+import { Head } from "./components/Head";
 
 export const App: FC = () => {
-  return <div></div>;
+  return (
+    <>
+      <Head />
+      <BrowserRouter>
+        <Routes>
+          {/* TODO: Pages */}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
