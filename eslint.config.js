90e38913de9dc5379a9e9a32eec44aeacd76a802
Eugene <88931804+eugenevve@users.noreply.github.com>
2026-07-12 00:19:18 +0600

    Adding the eslint library and prettier configuration: - Adding an eslint configuration with settings for the current project - Adding a prettier configuration with settings for the current project - Configuring the build command to pass the eslint check and adding the lint command for the check

---
+++ eslint.config.js
@@ -0,0 +1,81 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import importPlugin from "eslint-plugin-import";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  // 1. Basic settings
+  {
+    ignores: ["dist", "eslint.config.js", "vite.config.ts"],
+  },
+  js.configs.recommended,
+
+  // 2. Global TypeScript Configuration
+  ...tseslint.configs.recommendedTypeChecked.map((config) => ({
+    ...config,
+    languageOptions: {
+      ...config.languageOptions,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  })),
+
+  // 3. React settings
+  {
+    files: ["**/*.{jsx,tsx}"],
+    ...pluginReact.configs.flat.recommended,
+    ...pluginReact.configs.flat["jsx-runtime"],
+    settings: { react: { version: "detect" } },
+    rules: {
+      "react/jsx-key": "error",
+      "react/prop-types": "off",
+    },
+  },
+
+  // 4. Hooks and React Refresh
+  {
+    plugins: {
+      "react-hooks": pluginReactHooks,
+      "react-refresh": reactRefresh,
+      "@import": importPlugin,
+    },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "@import/order": [
+        "warn",
+        {
+          groups: ["builtin", "external", "internal", ["sibling", "parent"], "index", "unknown"],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+
+  // 5. General rules
+  {
+    languageOptions: {
+      globals: { ...globals.browser },
+    },
+    rules: {
+      eqeqeq: ["warn", "smart"],
+      "@typescript-eslint/no-floating-promises": "warn",
+      "@typescript-eslint/no-misused-promises": "warn",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["warn"],
+    },
+  },
+
+  eslintConfigPrettier,
+]);
