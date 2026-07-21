93cfc2d4a2eb140f83342254a6724fd9c7d9d451
Eugene <88931804+eugenevve@users.noreply.github.com>
2026-07-21 21:01:40 +0600

    Header refinement using new utils components and removing unnecessary styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,28 +1,22 @@
 import { Button } from "@app/ui/Button";
 import { ButtonKind } from "@app/ui/Button/Button.types";
-import { CodeIcon, HouseIcon, UserIcon } from "@app/ui/Icons";
+import { navigationItems } from "@app/utils/navigation";
+import { useNavigationKind } from "@app/utils/useNavigationKind";
 import type { FC } from "react";
-import { useLocation, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 
 import styles from "./Header.module.css";
 import { Logotype } from "../Logotype";
 
 export const Header: FC = () => {
   const navigate = useNavigate();
-  const { pathname } = useLocation();
-  const getButtonKind = (path: string) => (pathname === path ? ButtonKind.PRIMARY : ButtonKind.SECONDARY);
-
-  const items = [
-    { label: "Home", path: "/", icon: <HouseIcon /> },
-    { label: "Projects", path: "/projects", icon: <CodeIcon /> },
-    { label: "About", path: "/about", icon: <UserIcon /> },
-  ];
+  const getButtonKind = useNavigationKind(ButtonKind);
 
   return (
     <div className={styles.container}>
       <Logotype />
       <div className={styles.menu}>
-        {items.map(({ label, path, icon }) => (
+        {navigationItems.map(({ label, path, icon }) => (
           <Button key={path} kind={getButtonKind(path)} onClick={() => void navigate(path)} icon={icon}>
             {label}
           </Button>
