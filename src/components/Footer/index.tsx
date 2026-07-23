2f241caf5062fb7423cf0b8d84ca947ea17bfe6c
Eugene <88931804+eugenevve@users.noreply.github.com>
2026-07-24 00:56:18 +0600

    Adding a Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+import type { FC } from "react";
+
+import styles from "./Footer.module.css";
+
+export const Footer: FC = () => {
+  return <div className={styles.container}>© 2026 eugenevve. All rights reserved</div>;
+};
--- src/components/PageWrapper/index.tsx
+++ src/components/PageWrapper/index.tsx
@@ -4,6 +4,7 @@ import type { FC } from "react";
 import styles from "./PageWrapper.module.css";
 import type { IPageWrapper } from "./PageWrapper.types";
 import { BottomBar } from "../BottomBar";
+import { Footer } from "../Footer";
 import { Header } from "../Header";
 
 export const PageWrapper: FC<IPageWrapper> = ({ children }) => {
@@ -14,6 +15,7 @@ export const PageWrapper: FC<IPageWrapper> = ({ children }) => {
       <div className={styles.content}>
         {!isMobile && <Header />}
         <div className={styles.chapter}>{children}</div>
+        <Footer />
         {isMobile && <BottomBar />}
       </div>
     </div>
